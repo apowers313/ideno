97d264072f8749324b0235a953df6b01f40f58b9
Adam Powers <apowers@ato.ms>
2021-11-28 11:24:25 -0800

    refactor dependencies

---
+++ deps.ts
@@ -0,0 +1,4 @@
+// TODO: replace with Deno HMAC when available
+export { hmac } from "https://denopkg.com/chiefbiiko/hmac/mod.ts";
+export * as zmq from "https://raw.githubusercontent.com/apowers313/deno-zeromq/master/mod.ts";
+// export * as zmq from "/Users/ampower/Projects/personal/deno-zeromq/mod.ts";
