0a2b76a841b9236aa74de6fc2e2bf3548400735e
Adam Powers <apowers@ato.ms>
2021-12-09 22:30:10 -0800

    delint

--- deps.ts
+++ deps.ts
@@ -1,6 +1,4 @@
-export { BufReader, writeAll } from "https://deno.land/std@0.117.0/io/mod.ts";
+export { BufReader, readLines } from "https://deno.land/std@0.117.0/io/mod.ts";
 // TODO: replace with Deno HMAC when available
 export { hmac } from "https://denopkg.com/chiefbiiko/hmac/mod.ts";
 export * as zmq from "https://raw.githubusercontent.com/apowers313/deno-zeromq/master/mod.ts";
-// export * as zmq from "/Users/ampower/Projects/personal/deno-zeromq/mod.ts";
-export { readLines } from "https://deno.land/std@0.117.0/io/mod.ts";
