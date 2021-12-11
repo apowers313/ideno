9d5871167a49a1c37a7e2621b84933090fc0920f
Adam Powers <apowers@ato.ms>
2021-12-10 16:03:33 -0800

    remove hmac dependency

--- deps.ts
+++ deps.ts
@@ -1,4 +1,2 @@
 export { BufReader, readLines } from "https://deno.land/std@0.117.0/io/mod.ts";
-// TODO: replace with Deno HMAC when available
-export { hmac } from "https://denopkg.com/chiefbiiko/hmac/mod.ts";
 export * as zmq from "https://raw.githubusercontent.com/apowers313/deno-zeromq/master/mod.ts";
